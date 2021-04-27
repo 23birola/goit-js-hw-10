ad2242782571e92b3f1b25be69df4e961391df51
23birola <23birola@gmail.com>
2021-04-27 22:57:03 +0300

    added template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+import data from "./menu.json";
+
+const menuData = JSON.parse(data);
+const source = document.querySelector('#menu-template').innerHTML.trim();
+const template = Handlebars.compile(source);
+const markup = template(menuData);
+const container = document.querySelector('.js-menu');
+container.innerHTML = markup;
