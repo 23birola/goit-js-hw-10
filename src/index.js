d3bbab810a3774666d9078ba92667a400af1e972
23birola <23birola@gmail.com>
2021-04-28 21:50:04 +0300

    added theme switch

--- src/index.js
+++ src/index.js
@@ -6,3 +6,15 @@ import menuData from "./menu.json";
 const markup = template(menuData);
 const container = document.querySelector('.js-menu');
 container.insertAdjacentHTML('afterbegin', markup);
+
+
+const themeCheckbox = document.querySelector('.theme-switch__toggle');
+const page = document.querySelector('body');
+themeCheckbox.addEventListener('change', changeTheme);
+
+function changeTheme () {
+    page.classList.toggle("light-theme");
+    page.classList.toggle("dark-theme");
+    return;
+};
+ 
