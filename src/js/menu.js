d8f95f1dbdcdad5186219079c50829c588ebc710
23birola <23birola@gmail.com>
2021-04-30 18:55:42 +0300

    fixed

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,6 @@
 import template from "../templates/menu-template.hbs"
 import menuData from "../menu.json";
 
-
 const markup = template(menuData);
 const container = document.querySelector('.js-menu');
 container.insertAdjacentHTML('afterbegin', markup);
